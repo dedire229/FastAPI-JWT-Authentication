f13384a5daca6dc662ab7b8e9820341fe16432b6
raun1997 <raunakmitra7@gmail.com>
2026-06-16 00:33:55 +0530

    Added handler function for user logins that assigns each user tokens

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status, HTTPException, Depends
 from fastapi.responses import RedirectResponse
 from app.schemas import UserOut, UserAuth, TokenSchema
+from fastapi.security import OAuth2PasswordRequestForm
 from app.utils import (
     get_hashed_password,
     create_access_token,
@@ -29,3 +30,24 @@ async def create_user(data: UserAuth):
     }
     users_db[data.email] = user    # saving user to database
     return user
+
+@app.post('/login', summary="Create access and refresh tokens for user", response_model=TokenSchema)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = users_db.get(form_data.username, None)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect email or password"
+        )
+
+    hashed_pass = user['password']
+    if not verify_password(form_data.password, hashed_pass):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect email or password"
+        )
+
+    return {
+        "access_token": create_access_token(user['email']),
+        "refresh_token": create_refresh_token(user['email']),
+    }
